05a5880125763a00eb119abc243a282c10ca018c
nataliasirtak <nsirtakjob@gmail.com>
2023-05-29 12:17:22 -0500

    updated

--- src/main/java/com/solvd/laba/buildingcompany/connections/ConnectionPool.java
+++ src/main/java/com/solvd/laba/buildingcompany/connections/ConnectionPool.java
@@ -6,17 +6,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ConnectionPool {
-    // connection pool object is not created at class compilation, allowing for lazy initialization
+
     public static ConnectionPool connectionPool;
     private List<Connection> connections;
 
-    // constructor must be private
+
     private ConnectionPool() {
         connections = new ArrayList<>();
         initializeConnections();
     }
 
-    // ensures thread safety + single instance of com.solvd.connectionpool.Connection Pool
+
     public static synchronized ConnectionPool getInstance () {
         if (connectionPool == null) {
             connectionPool = new ConnectionPool();
@@ -25,7 +25,7 @@ public class ConnectionPool {
 
     }
 
-    // initializing 5 instances of com.solvd.connectionpool.Connection
+
     private void initializeConnections() {
         for (int i = 0; i < 5; i++) {
             Connection connection = new Connection("com.solvd.connectionpool.Connection"+i);
@@ -33,7 +33,7 @@ public class ConnectionPool {
         }
     }
 
-    // returns connection if the connections list is not empty
+
     public synchronized Connection getConnection () throws InterruptedException {
         while (connections.isEmpty()){
             wait();
@@ -41,7 +41,7 @@ public class ConnectionPool {
         Connection connection = connections.remove(connections.size()-1);
         return connection;
     }
-    // ads connection back to the list, notifies if waiting for connection
+
     public synchronized void releaseConnection(Connection connection){
         connections.add(connection);
         notify();
---
+++ src/main/java/com/solvd/laba/buildingcompany/connections/Main.java
@@ -0,0 +1,44 @@
+package com.solvd.laba.buildingcompany.connections;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                ConnectionPool connectionPool = ConnectionPool.getInstance();
+                Connection connection = null;
+                try {
+                    connection = connectionPool.getConnection();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                assert connection != null;
+                System.out.println(connection.getName());
+                connectionPool.releaseConnection(connection);
+            }
+        };
+
+
+        List<Thread> threadList = new ArrayList<>();
+        for (int i=0; i<7; i++){
+            threadList.add(new Thread(runnable));
+        }
+
+        ExecutorService executorService = Executors.newFixedThreadPool(7);
+
+        for (Thread t: threadList){
+            executorService.execute(t);
+        }
+        executorService.shutdown();
+
+    }
+}
+
+
+
